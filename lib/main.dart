80f406db0460685bb52a8e5325e72d192317431c
ibrahim-cse <ibrahim15-8607@diu.edu.bd>
2021-12-01 19:15:54 +0600

    Test project for smart_camera.

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:test_camera/editor.dart';
+import 'package:test_camera/slideshow.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Camera',
+    home: LandingScreen(),
+  ));
+}
+
+class LandingScreen extends StatefulWidget {
+  @override
+  _LandingScreenState createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  String dirPath = '';
+  File? imageFile;
+
+  _initialImageView() {
+    if (imageFile == null) {
+      return Text('Select Image');
+    } else {
+      return Card(child: Image.file(imageFile!, width: 400.0, height: 400));
+    }
+  }
+
+  _openGallery(BuildContext context) async {
+    var picture = await ImagePicker().pickImage(source: ImageSource.gallery);
+    setState(() {
+      imageFile = File(picture!.path);
+      dirPath = picture.path;
+      print('path');
+      print(dirPath);
+    });
+  }
+
+  _openCamera(BuildContext context) async {
+    var picture = await ImagePicker().pickImage(source: ImageSource.camera);
+    setState(() {
+      imageFile = picture as File;
+    });
+  }
+
+  Future<void> _showChoiceDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Take Image From...'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  GestureDetector(
+                    child: Text('Gallery'),
+                    onTap: () {
+                      _openGallery(context);
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  Padding(padding: EdgeInsets.all(8.0)),
+                  GestureDetector(
+                    child: Text('Camera'),
+                    onTap: () {
+                      _openCamera(context);
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Load Image'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            _initialImageView(),
+            Column(
+              children: [
+                RaisedButton(
+                  onPressed: () {
+                    _showChoiceDialog(context);
+                  },
+                  child: Column(
+                    children: [
+                      Text('Select Image'),
+                    ],
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Slideshow()),
+                    );
+                  },
+                  child: Text('Slideshow'),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (BuildContext context) {
+                        return Center(child: CircularProgressIndicator());
+                      },
+                    );
+                    new Future.delayed(new Duration(seconds: 2), () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ImagePainterExample(
+                                  filePath: dirPath,
+                                )),
+                      );
+                    });
+                  },
+                  child: Text('Image Editor'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
